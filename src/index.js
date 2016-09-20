21406e50fb8c0dad9a262a86dfc64554b748078f
qdvu124 <vudinhquan124@gmail.com>
2016-09-20 16:29:55 +0100

    Added video previewing functionalities

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyAiNyGgE5ntBES7fclYRfARCo22bazEj18';
 
-const App = () => {
-    return (
-         <div> 
-            <SearchBar />
-         </div>
-    );
-};
+class App extends React.Component {
+    constructor(props){
+        super(props);
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+        this.state = { 
+            videos: [],
+            selectedVideo: null,
+        };
+
+        YTSearch({key: API_KEY, term: 'kirameki'}, videos => {
+            this.setState({ 
+                videos,
+                selectedVideo: videos[0],
+             });
+            //this.setState({ videos: videos }), ES6 feature
+        }); 
+    }
+
+    render() {
+        return (
+            <div> 
+                <SearchBar />
+                <br/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
